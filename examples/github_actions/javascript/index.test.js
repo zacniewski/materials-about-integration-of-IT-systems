76623d67744d455c7860b31d30b3ae7a090c1c21
Artur Zacniewski <a.zacniewski@gmail.com>
2026-03-04 11:20:31 +0100

    Checking GitHub Actions for JavaScript

--- examples/github_actions/javascript/index.test.js
+++ examples/github_actions/javascript/index.test.js
@@ -15,3 +15,7 @@ test("adds 0 + 0 to equal 0", () => {
 test("adds 111 + 222 to equal 333", () => {
   expect(add(111, 222)).toBe(333);
 });
+
+test("adds 222 + 444 to equal 666", () => {
+  expect(add(222, 444)).toBe(666);
+});
